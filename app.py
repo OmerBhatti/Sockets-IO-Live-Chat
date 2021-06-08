588abd365f46e773c9974a0621a07094f35431ab
Omerbhatti <Omerbhatti34@gmail.com>
2021-06-08 14:26:25 +0500

    cross origin

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify;
 from flask_socketio import SocketIO
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yoo this is a secret. shhhhh!'
@@ -7,10 +8,12 @@ app.config['SECRET_KEY'] = 'yoo this is a secret. shhhhh!'
 socketIo = SocketIO(app,cors_allowed_origins="*")
 
 @app.route('/')
+@cross_origin()
 def status():
    return jsonify({"message":"Server is running!"})
 
 @socketIo.on("message")
+@cross_origin()
 def handleMessage(msg):
     socketIo.emit("renderer",msg)
 
