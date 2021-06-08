c0ef9abe68c5b0d5a3286839926ad15ca4971d64
Omerbhatti <Omerbhatti34@gmail.com>
2021-06-08 14:06:58 +0500

    CORS resolution

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, jsonify;
-from flask_socketio import SocketIO, send
-from flask_cors import CORS
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SECRET_KEY'] = 'yoo this is a secret. shhhhh!'
 
-socketIo = SocketIO(app)
+socketIo = SocketIO(app,cors_allowed_origins="*")
 
 @app.route('/')
 def status():
@@ -15,8 +12,7 @@ def status():
 
 @socketIo.on("message")
 def handleMessage(msg):
-    send(msg, broadcast=True)
-    return None
+    socketIo.emit("renderer",msg)
 
 if __name__ == '__main__':
     socketIo.run(app , debug=True)
