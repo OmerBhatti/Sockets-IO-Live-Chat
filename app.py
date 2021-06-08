8377fe4310926b20a360e4d099269d241694d4e9
Omerbhatti <Omerbhatti34@gmail.com>
2021-06-08 14:56:28 +0500

    Working locally but not on heroku

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify;
 from flask_socketio import SocketIO
-from flask_cors import cross_origin
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yoo this is a secret. shhhhh!'
@@ -8,12 +7,10 @@ app.config['SECRET_KEY'] = 'yoo this is a secret. shhhhh!'
 socketIo = SocketIO(app,cors_allowed_origins="*")
 
 @app.route('/')
-@cross_origin()
 def status():
    return jsonify({"message":"Server is running!"})
 
 @socketIo.on("message")
-@cross_origin()
 def handleMessage(msg):
     socketIo.emit("renderer",msg)
 
